6daf82bf1abb7a35026718fe30f4984c05a43311
Hannah Joynson <hannahjoynson@gmail.com>
2022-04-26 16:25:45 +0100

    adding files

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function play1() {
+var audio = document.getElementById("audio");
+audio.play();}
+
+function play2() {
+var audio = document.getElementById("audio1");
+audio.play();}
+
+function play3() {
+var audio = document.getElementById("audio2");
+audio.play();}
