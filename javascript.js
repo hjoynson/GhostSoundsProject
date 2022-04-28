a99964c3e00ecbee502dbd43051d393065b47a53
Hannah Joynson <hannahjoynson@gmail.com>
2022-04-28 10:45:53 +0100

    adding final changes

--- javascript.js
+++ javascript.js
@@ -9,3 +9,10 @@ audio.play();}
 function play3() {
 var audio = document.getElementById("audio2");
 audio.play();}
+
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode) {
+      document.getElementById('audio').play();
+    }
+  });
